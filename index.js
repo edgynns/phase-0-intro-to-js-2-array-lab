d4f3de82a59210a76004fd9c51a089a281c09a41
Edgar <muchemiedgar2@gmail.com>
2025-02-26 12:25:05 +0300

    Complete array methods lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+// Define the cats array with initial values
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive methods (modify the original array)
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+// Non-destructive methods (return new arrays)
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
